e2753ab09041145f604c867912505c07e49cd6ca
sangyeon kim <greyhairchooselife@gmail.com>
2025-11-12 05:06:25 +0900

    feat: ready_list에 넣을 때 priority 높은 순서로 넣기

--- threads/thread.c
+++ threads/thread.c
@@ -66,6 +66,9 @@ static tid_t allocate_tid(void);
 static bool sooner_first(const struct list_elem* a,
                          const struct list_elem* b,
                          void* _);
+static bool high_priority_first(const struct list_elem* a,
+                                const struct list_elem* b,
+                                void* _);
 
 /* Returns true if T appears to point to a valid thread. */
 #define is_thread(t) ((t) != NULL && (t)->magic == THREAD_MAGIC)
@@ -257,6 +260,17 @@ bool sooner_first(const struct list_elem* a,
     return false;
 }
 
+bool high_priority_first(const struct list_elem* a,
+                         const struct list_elem* b,
+                         void* _) {
+    int64_t wakeupA = list_entry(a, struct thread, elem)->priority;
+    int64_t wakeupB = list_entry(b, struct thread, elem)->priority;
+
+    if (wakeupB - wakeupA <= 0) return true;
+
+    return false;
+}
+
 /* Transitions a blocked thread T to the ready-to-run state.
    This is an error if T is not blocked.  (Use thread_yield() to
    make the running thread ready.)
@@ -272,7 +286,7 @@ void thread_unblock(struct thread* t) {
 
     old_level = intr_disable();
     ASSERT(t->status == THREAD_BLOCKED);
-    list_push_back(&ready_list, &t->elem);
+    list_insert_ordered(&ready_list, &t->elem, high_priority_first, NULL);
     t->status = THREAD_READY;
     intr_set_level(old_level);
 }
@@ -325,7 +339,9 @@ void thread_yield(void) {
     ASSERT(!intr_context());
 
     old_level = intr_disable();
-    if (curr != idle_thread) list_push_back(&ready_list, &curr->elem);
+    if (curr != idle_thread)
+        list_insert_ordered(&ready_list, &curr->elem, high_priority_first,
+                            NULL);
     do_schedule(THREAD_READY);
     intr_set_level(old_level);
 }
