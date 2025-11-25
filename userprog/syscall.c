02fdf380f7f3daddaa44743af6aeceaae1151da6
sangyeon kim <greyhairchooselife@gmail.com>
2025-11-26 04:34:07 +0900

    docs: syscall instruction에 대한 OS의 동작 설정 설명

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -24,14 +24,22 @@ void syscall_handler(struct intr_frame*);
 #define MSR_LSTAR 0xc0000082        /* Long mode SYSCALL target */
 #define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags */
 
+// %MSR은 syscall 관련 레지스터인데, 역할별로 여러개가 있다.
+//  이 함수에서는 3개의 %MSR을 세팅해준다.
 void syscall_init(void) {
+    // syscall, sysret instructions을 각각 호출할 때
+    // %cs, %ss의 CPL(Current Privilege Level)을 어떻게 설정할지 설정
     write_msr(MSR_STAR,
               ((uint64_t)SEL_UCSEG - 0x10) << 48 | ((uint64_t)SEL_KCSEG) << 32);
+
+    // syscall instruction 호출 진입점 설정
     write_msr(MSR_LSTAR, (uint64_t)syscall_entry);
 
     /* The interrupt service rountine should not serve any interrupts
      * until the syscall_entry swaps the userland stack to the kernel
      * mode stack. Therefore, we masked the FLAG_FL. */
+    // syscall instruction 호출 시 마스킹할 요소들
+    //  e.g Hardware Interrupts
     write_msr(MSR_SYSCALL_MASK,
               FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
 }
